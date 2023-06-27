1fc36218617c235b0972026f2875df6d344b5125
KimDongHu <ehdgn1022@naver.com>
2023-06-27 15:27:41 +0900

    두번 수정

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("아아 마이크 테스트 체키라웃");
 		System.out.println("깃허브테스트");
 		System.out.println("수정 한번 했읍니다.");
+		System.out.println("수정 두번 했읍니다.");
 	}
 }
